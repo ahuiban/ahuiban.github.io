0523e55487e1b40e06caa5f8f96a6b1ac6baea6a
Andrei <37360070+ahuiban@users.noreply.github.com>
2020-02-01 16:57:36 -0500

    updated

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 console.log("It's working")
 
 
+    (function ($) {
+        $(function () {
+
+            const myForm = {
+                // elements
+                _btn: $('#btn'),
+                _output: $('#output'),
+                _selAnswer: $('#selAnswers'),
+                _score: $('#score'),
+
+                // static
+                URL: 'https://opentdb.com/api.php?amount=',
+
+                // attrs
+                answers: {
+                    'correct': 0,
+                    'wrong': 0
+                },
+
+                init: function () {
+
+                    ths = this;
+                    ths._btn.on('click', ths.getNextItem);
+                    ths._selAnswer.on('click', ".ans", ths.checkAnswer);
+                },
+
+
+
+            }
